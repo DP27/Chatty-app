9e3681f57a7a3ee3451c79f314f0b7fc96f20bf3
DP27 <dishantpatel27@gmail.com>
2017-10-31 11:48:44 -0400

    first react component for chatty app

---
+++ src/Message.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+class Message extends Component {
+  render() {
+    return (
+    <main className="messages">
+    <MessageList />
+    <div className="message system">
+    </div>
+    </main>  
+    );
+  }
+}
+export default Message;
+
+
+
+
