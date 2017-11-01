460812a3afb5674e27ad204b71b6f207114b4f32
DP27 <dishantpatel27@gmail.com>
2017-11-01 19:58:16 -0400

    Implemented changing Username and displaying incoming messages functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,56 @@ import React, {Component} from 'react';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
+  
 
 class App extends Component {
+
   constructor(props){
     super(props);
+    this.socket;
     this.state = {
       loading :false,
-      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-        { id : "9bVde9",
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-        },
-        { id : "7abc7X",
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+      currentUser: {name: "Anonymous"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: []
     };  
   }
-   
-  newMessage = (messageNew) => {
-      this.state.messages.push({id:Math.random(),username:'Test',content:messageNew});
-      const message = this.state.messages
-      this.setState({messages:message});
-    }
-  
 
+
+  componentDidMount() {
+    let chattyWebSocket = new WebSocket("ws://localhost:3001/", "protocolOne");
+    chattyWebSocket.onopen = function(event){
+      console.log("Connected to the server.");
+    }
+    this.socket = chattyWebSocket;
+    this.socket.onmessage =(message) => {
+      var messageFromServer = message.data;
+      this.newMessageReceived(messageFromServer);
+    }
+  }
+  newMessage = (messageNew,user) => {
+    if(user){
+      this.state.currentUser.name = user;
+    }
+    var newMessageObj = {username:this.state.currentUser.name,content:messageNew};
+    console.log('message typed',messageNew);
+    //this.state.messages.push(newMessageObj);
+    this.socket.send(JSON.stringify(newMessageObj));
+    //const message = this.state.messages;
+    //this.setState({messages:message});
+   
+  }
+  // newUserName = (name) => {
+  //   this.state.currentUser.name = name;
   
+  // }
+
+  newMessageReceived = (messageNew) => {
+    console.log('received from server',messageNew);
+    this.state.messages.push(JSON.parse(messageNew));
+    const messageServer = this.state.messages;
+    console.log(messageServer);
+    this.setState({messages:messageServer});
+  }
 
 
   render() {
@@ -45,7 +67,7 @@ class App extends Component {
             <a href="/" className = "navbar-brand">Chatty</a>
           </nav>
           <MessageList messages={this.state.messages}/>
-          <ChatBar userName={this.state.currentUser.name} newMessage={this.newMessage}/>
+          <ChatBar userName={this.state.currentUser.name}  newMessage={this.newMessage} />
         </div>
       );
     }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+
+  username = null;
+  handleKeyPressUser = (e) => {
+  
+    this.username = e.target.value;
+    
+  }
   
  handleKeyPress = (e) =>{
    if(e.key === 'Enter'){
-     this.props.newMessage(e.target.value);
+     this.props.newMessage(e.target.value,this.username);
      e.target.value = null;
    }
  } 
+ 
  
 
   
@@ -15,7 +23,7 @@ class ChatBar extends Component {
     
     return (
     <footer className = "chatbar">
-        <input className = "chatbar-username" placeholder={this.props.userName} />
+        <input className = "chatbar-username" onChange = {this.handleKeyPressUser} placeholder={this.props.userName} />
         <input className = "chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={this.handleKeyPress}/>
     </footer>
       
